9bf20abc76fabb048dd7353e7e6557c0a8036aba
Bovince <42774968+Bovince@users.noreply.github.com>
2020-03-06 09:19:12 +0100

    Add files via upload
    6/3-20

--- main.php
+++ main.php
@@ -21,8 +21,9 @@
   </head>
   <header>
     <div class="header" id="header">
+      <div class="navbar" id="navbar">  </div>
       <h1> Hanatorps Camping </h1>
-      <div class="nav" onclick="menu()">
+      <div class="nav" onclick="menu()"> Menu
         <div id="hamburger" class="button_menu">
           <div id="line1" class="line_1"></div>
           <div id="line2" class="line_2"></div>
@@ -80,15 +81,31 @@
         </div>
       </div>
     </div>
-    <div class="navbar" id="navbar">  </div>
 
     <div class="info" id="info">
       <div class="panel_1">
+        <div class="bild_1">
+          <img src="./camping_1" alt="">
+          <img src="./utsikten" alt="">
+          <img src="./Logga" alt="">
+        </div>
         <div class="facts_p1">
-          <div><h3> Skriv o sätt in de som passar till den kategorin du arbetar med. Vi får fixa detta snabbt :P </h3></div>
+          <div class="text">
+            <h2> Hanatorps camping </h2>
+            <p> I natursköna Mark väntar mer än 300 sjöar och vattendrag på dig.</p>
+            <p> En lugn inlandsatmosfär råder i Hanatorp och stressen försvinner som sand mellan tårna när ni kommit fram till oss.
+            Natur och genuin glesbygd i ett vackert och spännande kulturlanskap. Det sammanfattar Öresjöbygden nära Örby på ett bra vis.</p>
+            <p>Hanatorp ligger precis invid Östra Öresjön och vid en av Västsveriges mysigaste och barnvänligaste strander.
+            Kom till oss på Hanatorp för naturnära avkoppling, äventyr eller lata dagar vid stranden.</p>
+            <p> Hanatorp har ett perfekt utgångsläge för västsvenska upptäcktsfärder.
+            50 minuter till Ullared, 50 minuter Göteborg, 30 minuter till Landvetter.</p>
+            <p> Vi önskar dig varmt välkommen till Hanatorps camping, nära Örby - Kinna. </p>
+          </div>
         </div>
       </div>
-
     </div>
-
   </body>
+  <footer>
+    <div class="footer"> okay </div>
+    <div class="footerbar"></div>
+  </footer>
